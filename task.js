4a39a8abff1bf71209f278cbf601adefe735bc73
A-Fierce <a.n.fierce@gmail.com>
2023-03-03 16:41:34 +0600

    First commit

---
+++ task.js
@@ -0,0 +1,10 @@
+const timer = document.getElementById("timer");
+const output = function (){
+	timer.textContent -= 1;
+	if (timer.textContent == 0){
+		alert ("Вы победили в конкурсе!")
+		clearInterval(timerId);
+	}
+	
+}	
+let timerId = setInterval(output, 1000);
